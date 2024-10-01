ba4a633c244858dc386a6d01a4f059e7db6f9d2b
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2024-10-01 16:32:08 +0100

    Tip Calculator challenge within steps - Appv2

--- steps/src/Appv2.js
+++ steps/src/Appv2.js
@@ -46,11 +46,15 @@ export default function App() {
         {isOpen ? '-' : '+'}
       </button>
       {isOpen && <Stepper />}
+
+      {/** FInally add in the tip calculator challenge */}
+      <TipChallenge />
     </>
   );
 }
 
 function Stepper() {
+  //bring the state with us into this component seeing as the parent component doesn't need to be aware of it ie keep it as far down the heirarchy as possible
   const [step, setStep] = useState(2);
 
   function handleNext() {
@@ -111,7 +115,7 @@ function Button({ textColour, bgColour, onClick, children }) {
 
 //--------------------------------------------------------------------------------
 
-//Here I'll pop the coding challenge component
+//Here I'll pop the step counter coding challenge component
 function PracticeCounter() {
   const [stepSize, setStepSize] = useState(1);
   const [counter, setCounter] = useState(0);
@@ -164,6 +168,87 @@ function PracticeCounter() {
           getDateString() + date.toLocaleDateString()
         }`}</p>
       </span>
+    </div>
+  );
+}
+
+//The Tip Calculator Challenge -----------------------------------------------
+function TipChallenge() {
+  const [tipOne, setTipOne] = useState(0);
+  const [tipTwo, setTipTwo] = useState(0);
+  const [billAmount, setBillAmount] = useState('');
+
+  const totalTip = (((tipOne + tipTwo) / 2) * Number(billAmount)).toFixed(2);
+
+  function resetTip() {
+    setTipOne(0);
+    setTipTwo(0);
+    setBillAmount('');
+  }
+
+  return (
+    <div>
+      <BillAmount onSetBillAmount={setBillAmount} bill={billAmount} />
+      <TipJudge onTipSelect={setTipOne} tipSelected={tipOne}>
+        What tip would you like to give?{' '}
+      </TipJudge>
+      <TipJudge onTipSelect={setTipTwo} tipSelected={tipTwo}>
+        What tip would your friend like to give?{' '}
+      </TipJudge>
+      {billAmount > 0 && (
+        <>
+          <TipCalculator amount={billAmount} tip={totalTip} />
+          <TipReset onTipReset={resetTip} />
+        </>
+      )}
+    </div>
+  );
+}
+
+function BillAmount({ onSetBillAmount, bill }) {
+  return (
+    <div style={{ color: '#000', fontFamily: 'sans - serif' }}>
+      <label>Enter the bill amount: </label>
+      <input
+        type="text"
+        placeholder="enter bill total"
+        value={bill}
+        onChange={(e) => onSetBillAmount(Number(e.target.value))}
+      ></input>
+    </div>
+  );
+}
+
+function TipJudge({ children, onTipSelect, tipSelected }) {
+  return (
+    <div style={{ color: '#000', fontFamily: 'sans - serif' }}>
+      <span>{children} </span>
+      <select
+        value={tipSelected}
+        onChange={(e) => onTipSelect(Number(e.target.value))}
+      >
+        <option value={0}>Really Poor</option>
+        <option value={0.05}>Quite Good</option>
+        <option value={0.1}>Very Good</option>
+        <option value={0.2}>Amazing</option>
+      </select>
+    </div>
+  );
+}
+function TipCalculator({ amount, tip }) {
+  return (
+    <h3 style={{ color: '#000', fontFamily: 'sans - serif' }}>
+      You pay £{(Number(amount) + Number(tip)).toFixed(2)} (£{amount} + £{tip})
+    </h3>
+  );
+}
+
+function TipReset({ onTipReset }) {
+  return (
+    <div>
+      <button type="reset" onClick={onTipReset}>
+        Reset
+      </button>
     </div>
   );
 }
