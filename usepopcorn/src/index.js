8b17770628e775c16a822086858aa00a16341c0c
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2024-10-08 16:27:16 +0100

    Added text expander challenge

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
+import TextExpanderApp from './textExpander';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 function testRating(rating) {
@@ -13,5 +14,6 @@ root.render(
     {/* <App /> */}
     <StarRating maxRating={10} colour="#ff0000" size={24} />
     <StarRating maxRating={5} onSetRating={testRating} />
+    <TextExpanderApp />
   </React.StrictMode>
 );
