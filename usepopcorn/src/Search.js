1b2b37655e9baf6ddd7828d36b5a099066532635
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2024-10-21 16:56:45 +0100

    adding key press event listener to the search bar focus functionality

--- usepopcorn/src/Search.js
+++ usepopcorn/src/Search.js
@@ -5,9 +5,29 @@ export function Search({ query, onSetQuery }) {
   //Due to the fact they will only be tied together when the DOM is loaded we useEffect
   const searchInput = useRef(null);
 
-  useEffect(function () {
-    searchInput.current.focus();
-  });
+  useEffect(
+    function () {
+      //To make the search bar gain focus on load we would do this
+      // searchInput.current.focus();
+      //but we want an example of using a key event to give focus so we'll use the add/remove event listener
+      function callBack(e) {
+        //we're already searching so ignore hitting enter
+        if (document.activeElement === searchInput.current) return;
+        //otherwise send focus to this search bar element
+        if (e.code === 'Enter') {
+          searchInput.current.focus();
+          onSetQuery('');
+        }
+      }
+      //run it to get focus on load...
+      callBack({ code: 'Enter' });
+      //and then whenever Enter key is pressed...
+      document.addEventListener('keydown', callBack);
+      //then use the clean up function so we don't add a new listener repeatedly
+      return () => document.removeEventListener('keydown', callBack);
+    },
+    [onSetQuery]
+  );
 
   return (
     <input
