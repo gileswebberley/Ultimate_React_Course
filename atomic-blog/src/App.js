b618316f968d584d34c1ff0ed13c6f3b4149d3e9
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2024-12-03 17:53:28 +0000

    memoisation of Archive component in App-memo plus extensive notes

--- atomic-blog/src/App.js
+++ atomic-blog/src/App.js
@@ -123,14 +123,17 @@ function FormAddPost() {
 function List() {
   const { posts } = useBlogContext();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test /> */}
+    </>
   );
 }
 
