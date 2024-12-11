3000c1ea7d153cd33d3003440825469785e1073b
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2024-12-11 17:39:42 +0000

    customerSlice also converted to use the RTK method

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -57,13 +57,16 @@ const accountSlice = createSlice({
   },
 });
 
-console.log(accountSlice);
+//console.log(accountSlice);
 
 //now destructure the actions object to name export the action creators
 export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
-//We'll use our old Thunk solution instead of exporting the deposit action - careful to name the type correctly ie 'name/action'
-export function deposit(amount, currency) {
+//We'll use our old Thunk solution instead of exporting the deposit action to avoid the more complex RTK way of using createAsyncThunk - careful to name the type correctly ie 'name/action' (another good reason for the fake enum)
+export function deposit(
+  amount,
+  currency = initialStateAccount.nativeCurrencyCode
+) {
   //If the currency is the native currency work as before
   if (currency === initialStateAccount.nativeCurrencyCode)
     return { type: AccountActions.DEPOSIT, payload: amount };
@@ -85,82 +88,3 @@ export function deposit(amount, currency) {
 }
 //default export our reducer
 export default accountSlice.reducer;
-
-// export default function accountReducer(state = initialStateAccount, action) {
-//   switch (action.type) {
-//     //action names should be it's domain then model what has or is happening
-//     case AccountActions.DEPOSIT:
-//       //console.log(`DEPOSIT: ${action.payload}`);
-//       return {
-//         ...state,
-//         balance: state.balance + Number(action.payload),
-//         isLoading: false,
-//       };
-
-//     case AccountActions.WITHDRAW:
-//       return { ...state, balance: state.balance - Number(action.payload) };
-
-//     case AccountActions.LOAN_REQUEST:
-//       if (state.loan > 0) return state;
-//       return {
-//         ...state,
-//         loan: Number(action.payload.amount),
-//         loanPurpose: action.payload.purpose,
-//         balance: state.balance + Number(action.payload.amount),
-//       };
-
-//     case AccountActions.LOAN_PAY:
-//       return {
-//         ...state,
-//         loan: initialStateAccount.loan,
-//         loanPurpose: initialStateAccount.loanPurpose,
-//         balance: state.balance - state.loan,
-//       };
-
-//     case AccountActions.CONVERTING:
-//       return { ...state, isLoading: true };
-
-//     default:
-//       //in Redux it's recommended that you do not throw an error
-//       //console.log('account reducer called with undefined action type');
-//       return state;
-//   }
-// }
-
-// //a useful convention is to use action creators, an example for the loan -
-// export function requestLoan(amount, purpose) {
-//   return {
-//     type: AccountActions.LOAN_REQUEST,
-//     payload: { amount, purpose },
-//   };
-// }
-
-// export function payLoan() {
-//   return { type: AccountActions.LOAN_PAY };
-// }
-
-// //We are going to implement our use of THUNK by returning a function if we need to call the currency conversion API instead of the dispatchable object
-// export function deposit(amount, currency) {
-//   //If the currency is the native currency work as before
-//   if (currency === initialStateAccount.nativeCurrencyCode)
-//     return { type: AccountActions.DEPOSIT, payload: amount };
-
-//   //otherwise we automagically call the THUNK middleware with an async function that does it's own dispatch when data arrives
-//   return async function (dispatch, getState) {
-//     dispatch({ type: AccountActions.CONVERTING });
-//     //call the API (frankfurter.dev)
-//     const res = await fetch(
-//       `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=${initialStateAccount.nativeCurrencyCode}`
-//     );
-//     const data = await res.json();
-//     const converted = (
-//       amount * data.rates[initialStateAccount.nativeCurrencyCode]
-//     ).toFixed(2);
-//     //now dispatch the action
-//     dispatch({ type: AccountActions.DEPOSIT, payload: converted });
-//   };
-// }
-
-// export function withdraw(amount) {
-//   return { type: AccountActions.WITHDRAW, payload: amount };
-// }
--- redux-intro/src/features/customers/customerSlice.js
+++ redux-intro/src/features/customers/customerSlice.js
@@ -1,41 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 //Initial State
 const initialStateCustomer = {
   fullName: '',
   nationalID: '',
   createdAt: '',
 };
-//Fake Enum
-const CustomerActions = {
-  CREATE: 'customer/createCustomer',
-  CHANGE_NAME: 'customer/changeName',
-};
 
-//Reducer - export as the default
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case CustomerActions.CREATE:
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case CustomerActions.CHANGE_NAME:
-      return { ...state, fullName: action.payload };
-    default:
-      //console.log('customer reducer called with undefined action type');
-      return state;
-  }
-}
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState: initialStateCustomer,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        //const createTime = new Date().toISOString();
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    changeName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-//Action creators - export as named exports
+export const { createCustomer, changeName } = customerSlice.actions;
+export default customerSlice.reducer;
 
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: CustomerActions.CREATE,
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-export function changeName(fullName) {
-  return { type: CustomerActions.CHANGE_NAME, payload: fullName };
-}
+//see v1 for old (pre RTK) way of doing this
