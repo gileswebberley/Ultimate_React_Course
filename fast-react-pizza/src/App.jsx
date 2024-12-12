0a209e8a1c7bd539024b666a3e81794772522113
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2024-12-12 17:22:28 +0000

    App layout and basic routes being provided to the Outlet component

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -6,32 +6,40 @@ import Menu from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
 
 //Let's build out our Routes in an imperative way, unlike in our WorldWise app, so that we can do data loading in this way
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '/order/new',
-    element: <CreateOrder />,
-  },
-  {
-    path: '/order/:orderId',
-    element: <Order />,
-  },
-]);
-
-//Now we're going to create a layout that will work with phone screens or browsers on a pc
+const router = createBrowserRouter(
+  //Now we're going to create a layout that will work with phone screens or browsers on a pc in the AppLayout component, then make our routes children
+  [
+    {
+      //layout so no path
+      element: <AppLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/menu',
+          element: <Menu />,
+        },
+        {
+          path: '/cart',
+          element: <Cart />,
+        },
+        {
+          path: '/order/new',
+          element: <CreateOrder />,
+        },
+        {
+          path: '/order/:orderId',
+          element: <Order />,
+        },
+      ],
+    },
+  ]
+);
 
 function App() {
   //Now we can inject our router into our SPA by doing the following
--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,5 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+
+//See App for the Routes that are placed in the Outlet component
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
