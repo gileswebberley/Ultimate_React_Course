5aebeb9f4b97a06e46ecce144dc2ba474f261dc2
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2024-12-13 15:33:02 +0000

    loader functionality added to Order and SearchOrder created

--- fast-react-pizza/src/features/order/Order.jsx
+++ fast-react-pizza/src/features/order/Order.jsx
@@ -1,47 +1,50 @@
 // Test ID: IIDSAT
 
+import { useLoaderData } from 'react-router-dom';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from "../../utils/helpers";
+} from '../../utils/helpers';
+import { getOrder } from '../../services/apiRestaurant';
 
 const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
+  id: 'ABCDEF',
+  customer: 'Jonas',
+  phone: '123456789',
+  address: 'Arroios, Lisbon , Portugal',
   priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
+  estimatedDelivery: '2027-04-25T10:00:00',
   cart: [
     {
       pizzaId: 7,
-      name: "Napoli",
+      name: 'Napoli',
       quantity: 3,
       unitPrice: 16,
       totalPrice: 48,
     },
     {
       pizzaId: 5,
-      name: "Diavola",
+      name: 'Diavola',
       quantity: 2,
       unitPrice: 16,
       totalPrice: 32,
     },
     {
       pizzaId: 3,
-      name: "Romana",
+      name: 'Romana',
       quantity: 1,
       unitPrice: 15,
       totalPrice: 15,
     },
   ],
-  position: "-9.000,38.000",
+  position: '-9.000,38.000',
   orderPrice: 95,
   priorityPrice: 19,
 };
 
 function Order() {
+  const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -69,7 +72,7 @@ function Order() {
         <p>
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
+            : 'Order should have arrived'}
         </p>
         <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
@@ -81,6 +84,16 @@ function Order() {
       </div>
     </div>
   );
+}
+
+//using the loader Step 1 - create this function (in the file by default)
+//step 2 - import into the App (or where the Browser Router is defined) eg import Order, { loader as orderLoader } from './features/order/Order';
+//Step 3 - define it as the loader parameter in the route definition object eg {path: '/order/:orderId', element: <Order />, loader: orderLoader,}
+export async function loader({ params }) {
+  //const { orderId } = useParams(); This doesn't work because it is not a react component, however loader() takes params as an argument
+  const data = await getOrder(params.orderId);
+  //console.log(`Order params: ${JSON.stringify(params)}`);
+  return data;
 }
 
 export default Order;
