3cd926cc6349918403b63fb12f5e59a06ee78952
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-03-04 14:27:41 +0000

    react-error-boundary installed and implemented for error fallback behaviour

--- wild-oasis/src/ui/ErrorFallback.jsx
+++ wild-oasis/src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Heading from './Heading';
+import GlobalStyles from '../styles/GlobalStyles';
+import Button from './Button';
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -19,13 +22,39 @@ const Box = styled.div`
   flex: 0 1 96rem;
   text-align: center;
 
-  & h1 {
+  & h2 {
     margin-bottom: 1.6rem;
   }
 
   & p {
-    font-family: "Sono";
+    font-family: 'Courier';
     margin-bottom: 3.2rem;
     color: var(--color-grey-500);
   }
 `;
+
+//for use with the react-error-boundary package which automagically sends through the error prop. We have also defined a callback function in our ErrorBoundary so that will be passed through as the resetErrorBoundary prop
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    // We are now outside of our App (because an error has occurred which brought us to this page) so we need to import those global styles like we did for App
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h2">
+            We&apos;re very sorry but something has gone wrong
+          </Heading>
+          <p>{error.message}</p>
+          <Button
+            style={{ width: 'fit-content', justifySelf: 'center' }}
+            onClick={resetErrorBoundary}
+          >
+            Return to Home
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
